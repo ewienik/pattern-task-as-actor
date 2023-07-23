0e16cb6b9629bbe9fff6694fe063363968536ed5
Pawel Pery <ewienik@post.pl>
2023-07-23 11:46:15 +0200

    implemented Supervisor, Ping, Pong

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # pattern-task-as-actor
 Actor model using simple tokio tasks
+
+# usage
+
+```
+cargo build
+cargo r
+```
+
+## Design
+
+There are three actors:
+- Supervisor: stores and stops handles to all actors
+- Ping: sends pong to Pong and stops Supervisor when after 10 balls
+- Pong: sends ping to Ping
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,198 @@
-use {
-    futures::StreamExt,
-    tokio::{
-        sync::{mpsc, oneshot},
-        task::JoinHandle,
-    },
-    tokio_stream::wrappers::ReceiverStream,
-};
+use {ping::Ping, pong::Pong, supervisor::Supervisor, tokio::task::JoinHandle};
 
-enum Message {
-    One { tx: oneshot::Sender<String> },
-    Two,
-}
+pub type ActorHandle = JoinHandle<()>;
 
 #[async_trait::async_trait]
-trait MessageExt {
-    async fn one(&self) -> String;
-    async fn two(&self);
+pub trait Stop {
+    async fn stop(&self);
 }
 
-#[async_trait::async_trait]
-impl MessageExt for mpsc::Sender<Message> {
-    async fn one(&self) -> String {
-        let (tx, rx) = oneshot::channel();
-        self.send(Message::One { tx }).await.unwrap();
-        rx.await.unwrap()
-    }
-    async fn two(&self) {
-        self.send(Message::Two).await.unwrap();
-    }
-}
+mod supervisor {
+    use {
+        super::*,
+        futures::{future, stream, StreamExt},
+        tokio::sync::mpsc::{self, Sender},
+    };
 
-fn new_task() -> (mpsc::Sender<Message>, JoinHandle<()>) {
-    let (tx, rx) = mpsc::channel(10);
-    let task = tokio::spawn(async move {
-        println!("Started task");
-        ReceiverStream::new(rx)
-            .for_each(|msg| async move {
-                match msg {
-                    Message::One { tx } => {
-                        println!("Received one!");
-                        tx.send("Hello!".to_string()).unwrap();
-                    }
-                    Message::Two => {
-                        println!("Received two!");
+    enum Message {
+        Attach {
+            actor: Box<dyn Stop + Send + Sync>,
+            handle: ActorHandle,
+        },
+        Stop,
+    }
+
+    #[derive(Clone)]
+    pub struct Supervisor(Sender<Message>);
+
+    impl Supervisor {
+        pub async fn attach(&self, actor: Box<dyn Stop + Send + Sync>, handle: ActorHandle) {
+            self.0
+                .send(Message::Attach { actor, handle })
+                .await
+                .unwrap();
+        }
+
+        pub fn new() -> (Supervisor, ActorHandle) {
+            let (tx, mut rx) = mpsc::channel(10);
+            let task = tokio::spawn(async move {
+                println!("supervisor: start task");
+                let mut actors = Vec::new();
+                let mut handles = Vec::new();
+                while let Some(msg) = rx.recv().await {
+                    match msg {
+                        Message::Attach { actor, handle } => {
+                            actors.push(actor);
+                            handles.push(handle);
+                        }
+                        Message::Stop => rx.close(),
                     }
                 }
-            })
-            .await;
-        println!("Finished task");
-    });
-    (tx, task)
+                stream::iter(actors.into_iter())
+                    .for_each(|actor| async move {
+                        actor.stop().await;
+                    })
+                    .await;
+                future::join_all(handles.into_iter()).await;
+                println!("supervisor: finish task");
+            });
+            (Supervisor(tx), task)
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl Stop for Supervisor {
+        async fn stop(&self) {
+            self.0.send(Message::Stop).await.unwrap();
+        }
+    }
+}
+
+mod ping {
+    use {
+        super::*,
+        tokio::sync::mpsc::{self, Sender},
+    };
+
+    enum Message {
+        Start { pong: Pong },
+        Ping,
+        Stop,
+    }
+
+    #[derive(Clone)]
+    pub struct Ping(Sender<Message>);
+
+    impl Ping {
+        pub async fn start(&self, pong: Pong) {
+            self.0.send(Message::Start { pong }).await.unwrap();
+        }
+
+        pub async fn ping(&self) {
+            self.0.send(Message::Ping).await.unwrap();
+        }
+
+        pub fn new(supervisor: Supervisor) -> (Ping, ActorHandle) {
+            let (tx, mut rx) = mpsc::channel(10);
+            let task = tokio::spawn({
+                let tx = tx.clone();
+                async move {
+                    println!("ping: start task");
+                    let mut my_pong = None;
+                    let mut counter = 0;
+                    while let Some(msg) = rx.recv().await {
+                        match msg {
+                            Message::Start { pong } => {
+                                my_pong.replace(pong);
+                                tx.send(Message::Ping).await.unwrap();
+                            }
+                            Message::Ping => {
+                                counter += 1;
+                                if counter == 10 {
+                                    println!("done");
+                                    supervisor.stop().await;
+                                } else {
+                                    print!("ping..");
+                                    my_pong.as_ref().unwrap().pong().await;
+                                }
+                            }
+                            Message::Stop => {
+                                rx.close();
+                            }
+                        }
+                    }
+                    println!("ping: finish task");
+                }
+            });
+            (Ping(tx), task)
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl Stop for Ping {
+        async fn stop(&self) {
+            self.0.send(Message::Stop).await.unwrap();
+        }
+    }
+}
+
+mod pong {
+    use {
+        super::*,
+        tokio::sync::mpsc::{self, Sender},
+    };
+
+    enum Message {
+        Pong,
+        Stop,
+    }
+
+    #[derive(Clone)]
+    pub struct Pong(Sender<Message>);
+
+    impl Pong {
+        pub async fn pong(&self) {
+            self.0.send(Message::Pong).await.unwrap();
+        }
+
+        pub fn new(ping: Ping) -> (Pong, ActorHandle) {
+            let (tx, mut rx) = mpsc::channel(10);
+            let task = tokio::spawn(async move {
+                println!("pong: start task");
+                while let Some(msg) = rx.recv().await {
+                    match msg {
+                        Message::Pong => {
+                            print!("pong..");
+                            ping.ping().await;
+                        }
+                        Message::Stop => {
+                            rx.close();
+                        }
+                    }
+                }
+                println!("pong: finish task");
+            });
+            (Pong(tx), task)
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl Stop for Pong {
+        async fn stop(&self) {
+            self.0.send(Message::Stop).await.unwrap();
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let (tx, task) = new_task();
-
-    println!("Received: {}", tx.one().await);
-    tx.two().await;
-    println!("Received: {}", tx.one().await);
-    tx.two().await;
-    drop(tx);
-    task.await.unwrap();
+    let (supervisor, supervisor_task) = Supervisor::new();
+    let (ping, ping_task) = Ping::new(supervisor.clone());
+    supervisor.attach(Box::new(ping.clone()), ping_task).await;
+    let (pong, pong_task) = Pong::new(ping.clone());
+    supervisor.attach(Box::new(pong.clone()), pong_task).await;
+    ping.start(pong).await;
+    supervisor_task.await.unwrap();
 }
 
 #[cfg(test)]
